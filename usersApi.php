2565616934d300bffb0f29eecb8341f6f0765acf
DevRubia <78357146+DevRubia@users.noreply.github.com>
2023-07-13 16:19:40 +0300

    Add files via upload

--- usersApi.php
+++ usersApi.php
@@ -3,11 +3,13 @@ include('conndb.php');
 $users=$auth->listUsers();
 $i=1;
  foreach($users as $row){
-    header('Content-Type: application/json'); 
+header('Content-Type: application/json'); 
 
 echo json_encode($row);
 //separate each json
 echo "\n"; 
 };
 
+
+								
 ?>
